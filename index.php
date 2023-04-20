928025ca547855ad543fdd0ce278f08df8209077
Anthony Luis <anthonyluis1730@gmail.com>
2023-04-20 15:46:16 -0500

    Added Site 1

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/styles.css" />
+</head>
+<body>
+    <header>
+        <div class="container  mx-auto flex justify-between mt-5">
+            <div>
+                SMASHINGDzine
+            </div>
+            <div>
+                +971 55 7457383
+            </div>
+        </div>
+    </header>
+
+    <nav class="container mx-auto mt-8">
+        <ul class="flex gap-9">
+            <li><a href=""></a>HOME</li>
+            <li><a href=""></a>ABOUT SMASHINGDZINE</li>
+            <li><a href=""></a>OUR SERVICES</li>
+            <li><a href=""></a>OUR PORTFOLIO</li>
+            <li><a href=""></a>BLOG</li>
+            <li><a href=""></a>CONTACTUS</li>
+        </ul>
+    </nav>
+
+    <main class="mt-10">
+        <div class="container mx-auto">
+
+            <div class="flex">
+                <div class="w-1/2">
+                <img src="https://www.webfx.com/wp-content/uploads/2021/10/04-10_code_smashing_design.jpg">
+                </div>
+
+                <div class="w-1/2">
+                    <h4 class="mt-6 text-4xl">Quick Quote</h4>
+
+                    <h6>Your full name:</h6>
+
+                    <h6>Your Email:</h6>
+
+                    <h6>What Are You Looking For:</h6>
+
+                    <div>
+                        <button class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                            Submit
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Posts -->
+            <div class="mt-10">
+                <div id="posts" class="grid grid-cols-3 gap-6">
+
+                    <!-- Posts 1 -->
+                    <div class="container mx-auto">
+                        <article class="my-8">
+                            <h1 class="text-4xl mb-4">About SMASHINGDzine</h1>
+                            <p class="w-1/2 float-left mr-4 mt-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione molestias dignissimos nam laudantium consequatur quo cum incidunt voluptas necessitatibus. Odit pariatur nesciunt asperiores nobis totam. Tempore fugit commodi impedit.</p>
+                            <img src="https://fondosmil.com/fondo/3879.jpg" alt="Descripción de la imagen" class="w-1/2 float-right">
+                            <a href="#" class="block bg-green-500 text-white py-2 px-4 mt-4">Read More</a>
+                        </article>
+                    </div>
+                    <!-- Posts 2 -->
+                    <div class="container mx-auto">
+                        <article class="my-8">
+                            <h1 class="text-4xl mb-4">Our Services</h1>
+                            <p class="w-1/2 float-left mr-4 mt-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione molestias dignissimos nam laudantium consequatur quo cum incidunt voluptas necessitatibus. Odit pariatur nesciunt asperiores nobis totam. Tempore fugit commodi impedit.</p>
+                            <img src="https://fondosmil.com/fondo/3879.jpg" alt="Descripción de la imagen" class="w-1/2 float-right">
+                            <a href="#" class="block bg-green-500 text-white py-2 px-4 mt-4">Read More</a>
+                        </article>
+                    </div>
+                    <!-- Posts 3 -->
+                    <div class="container mx-auto">
+                        <article class="my-8">
+                            <h1 class="text-4xl mb-4">Our Portfolio</h1>
+                            <p class="w-1/2 float-left mr-4 mt-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione molestias dignissimos nam laudantium consequatur quo cum incidunt voluptas necessitatibus. Odit pariatur nesciunt asperiores nobis totam. Tempore fugit commodi impedit.</p>
+                            <img src="https://fondosmil.com/fondo/3879.jpg" alt="Descripción de la imagen" class="w-1/2 float-right">
+                            <a href="#" class="block bg-green-500 text-white py-2 px-4 mt-4">Read More</a>
+                        </article>
+                    </div>
+
+                </div>
+
+                <div id="sidebar">
+
+                </div>
+
+            </div>
+
+        </div>
+    </main>
+
+        <footer class="mt-10" style="background-color: black; color: white; padding: 20px;">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-6">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit pariatur quisquam ipsam voluptatibus et esse totam, dignissimos consequuntur dicta! Voluptatibus deleniti iusto ab impedit veritatis sequi ex beatae id deserunt.</p>
+        </div>
+        <div class="col-md-6">
+            <ul class="flex">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">/About SmashingDzine</a></li>
+            <li><a href="#">/Our Services</a></li>
+            <li><a href="#">/Portfolio</a></li>
+            <li><a href="#">/Blog</a></li>
+            <li><a href="#">/Contact Us</li>
+            </ul>
+        </div>
+        </div>
+    </div>
+    </footer>
+</body>
+</html>
